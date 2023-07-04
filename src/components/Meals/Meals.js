25e28548f034eca0a0fea7e1c0a80329a3b46474
Prajwal Choudhary <prajwalchoudhary14@gmail.com>
2023-07-04 16:14:04 +0530

    1. feat: added checkout form component to submit user's and cart items to mock firebase backend

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,38 +1,66 @@
 import MealsSummary from "./MealsSummary";
 import AvailableMeals from "./AvailableMeals";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import classes from "./Meals.module.css";
+import { useEffect, useState, useCallback } from "react";
 
 const Meals = (props) => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMeals = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(
+        "https://react-http-479d8-default-rtdb.firebaseio.com/meals.json"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      const mealsData = [];
+      Object.entries(data).forEach(([key, value]) => {
+        const mealObj = {
+          id: key,
+        };
+        Object.entries(value).forEach(([key, value]) => {
+          mealObj[key] = value;
+        });
+        mealsData.push(mealObj);
+        setMeals(mealsData);
+      });
+    } catch (e) {
+      setIsLoading(false);
+      setError(e.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMeals();
+  }, [fetchMeals]);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{error}</p>
+      </section>
+    );
+  }
+
   return (
     <>
       <MealsSummary />
-      <AvailableMeals meals={DUMMY_MEALS} />
+      <AvailableMeals meals={meals} />
     </>
   );
 };
